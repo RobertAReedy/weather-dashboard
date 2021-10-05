d5db27f7532862d0ef02adbec77c4b3300b0430e
RobertAReedy <robertandreedy@gmail.com>
2021-10-05 17:23:55 -0400

    oops accidentally made it get data, but from a database that doesn't have everything I need, so instead I made the search history work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var weatherInfo;
 var apiKey = "f8e665afb7df296fa3ac06027ab4d95d";
+var historyButtons = [];
 
 /**
  * Creates and appends the history buttons that will eventually display
@@ -8,27 +9,37 @@ var apiKey = "f8e665afb7df296fa3ac06027ab4d95d";
  */
 function setHistoryButtons() {
     console.log("setHistoryButtons called");
+    for (var i = 0; i < 7; i++) {
+        buttonEl = document.createElement("button");
+        buttonEl.className = "city-history-button hide";
+        historyButtons.push(buttonEl);
+        $("#city-search-history-container").append(buttonEl);
+    }
 }
 
 /**
  * Will call the database, grab the necessary data, and stick it in a big variable
  * called weatherInfo
  */
-var getWeather = function(cityName) {
+var getCurrentWeather = function(cityName) {
     console.log("getWeather activated; value: " + cityName);
-    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
 
-    fetch(apiUrl);
-    // .then(function(response) {
-    //     if (response.ok) {
-    //         response.json().then(function(data) {
-    //             console.log(data);
-    //         });
-    //     }
-    //     else {
-    //         console.log("fetch failed");
-    //     }
-    // });
+    fetch(apiUrl)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log("Temp in fahrenheit: " + data.main.temp);
+                weatherInfo = data;
+
+                setCurrentWeather(); //begin next function in chain
+                updateSearchHistory();
+            });
+        }
+        else {
+            console.log("fetch failed");
+        }
+    });
 }
 
 /**
@@ -43,7 +54,12 @@ function getWeatherImage(weather) {
  */
 function setCurrentWeather() {
     console.log("setCurrentWeather activated");
-    getWeatherImage("weather goes here");
+    $("#current-weather-title").text($("#city-search-input").val());
+    $("#current-weather-temp").text("Temp: " + weatherInfo.main.temp);
+    $("#current-weather-wind").text("Wind: " + weatherInfo.wind.speed + " mph");
+    $("#current-weather-humidity").text("Humidity: " + weatherInfo.main.humidity + " %");
+    $("#current-weather-uv").text("dunno man");
+    // getWeatherImage("weather goes here");
 }
 
 /**
@@ -54,16 +70,36 @@ function setForecast() {
 }
 
 /**
- * 
+ * Updates the column of search history buttons using the
+ * historyButtons array
  */
 function updateSearchHistory() {
     console.log("updateSearchHistory activated");
+    for (var i = 0; i < historyButtons.length; i++) {
+        if (historyButtons[i].textContent === $("#city-search-input").val()) {
+            console.log("repeat; loop ended");
+            return;
+        }
+    }
+    for (var i = historyButtons.length - 1; i > 0; i--) {
+        historyButtons[i].textContent = historyButtons[i-1].textContent;
+        if (historyButtons[i].textContent) {
+            historyButtons[i].setAttribute("class", "city-history-button");
+        }
+    }
+    historyButtons[0].setAttribute("class", "city-history-button");
+    historyButtons[0].textContent = $("#city-search-input").val();
 }
 
-$("#city-search-button").click(function() {
-    $("#city-search-input").val("Richmond");
+setHistoryButtons();
 
-    getWeather($("#city-search-input").val());
-    setCurrentWeather();
-    setForecast();
+$("#city-search-button").click(function() {
+    // $("#city-search-input").val("Richmond");
+
+    getCurrentWeather($("#city-search-input").val())
+});
+
+$("#city-search-history-container").on("click", ".city-history-button", function() {
+    $("#city-search-input").val($(this).text());
+    getCurrentWeather($("#city-search-input").val());
 });
